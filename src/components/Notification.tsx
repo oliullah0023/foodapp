e75af06ead2f77c8356306069cc3b849feab56d1
Oliullah <oliullahservice@gmail.com>
2023-12-13 17:25:08 +0600

    first commit

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Notification = () => {
   return (
     <div className=' h-12 bg-red-500 text-white text-center flex items-center justify-center cursor-pointer text-sm md:text-base'>
-      <marquee behavior="scroll" direction="roght" >Free delivery for over $50 dollar</marquee>
+      <h2  >Free delivery for over $50 dollar</h2>
     </div>
   )
 }
